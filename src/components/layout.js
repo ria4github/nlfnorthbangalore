b777ecdc41266484030c3c6fc4aded964fb1bd49
onlyveen <praveenasvenom@gmail.com>
2020-01-21 12:48:01 +0530

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
 
 import Header from "./Header";
+import Header from "./Footer";
 import "../sass/main.scss";
 
 const Layout = ({ children }) => {
