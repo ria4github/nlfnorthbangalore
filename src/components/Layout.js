f4f22abc8ed2574f18f636a5bbf7ad5179f23854
onlyveen <praveenasvenom@gmail.com>
2020-01-21 19:54:57 +0530

    build fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,16 +10,14 @@ import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
 
 import Header from "./Header";
-import Header from "./Footer";
+import Footer from "./Footer";
 import "../sass/main.scss";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
+      site(siteMetadata: { title: {} }) {
+        id
       }
     }
   `);
