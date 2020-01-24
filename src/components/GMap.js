595d803a5e6037b081294d1ce8e4f1e506b10b30
onlyveen <praveenasvenom@gmail.com>
2020-01-24 09:27:39 +0530

    header styling fix throuhh layout

---
+++ src/components/GMap.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const GMap = () => {
+  return (
+    <div className="gmaps">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2028.2420432042334!2d77.58241697045878!3d13.093339008740084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1867aed02b69%3A0x800c1bae3b83462b!2sNew%20Life%20Fellowship%20Yelahanka%20Church!5e0!3m2!1sen!2sin!4v1579837418418!5m2!1sen!2sin"
+        frameBorder="0"
+        allowFullScreen=""
+        title="maps"
+      ></iframe>
+    </div>
+  );
+};
